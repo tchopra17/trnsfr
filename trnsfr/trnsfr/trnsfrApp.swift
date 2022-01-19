57c7c24736be8b917e7980636d580adb52338036
Tushar Chopra <tusharchopra17@gmail.com>
2022-01-18 21:28:15 -0800

    added apple-app-site-association

---
+++ trnsfr/trnsfr/trnsfrApp.swift
@@ -0,0 +1,17 @@
+//
+//  trnsfrApp.swift
+//  trnsfr
+//
+//  Created by Tushar Chopra on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct trnsfrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
